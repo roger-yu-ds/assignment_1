7461d9c9cc476286ac191247062345bc2789079d
Roger Yu <rogeryu@hotmail.com.au>
2021-01-29 21:12:25 +1100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MDSI 36114 ADSI Assignment 1',
+    author='Mark Brackenrig, Priyanka Srinivasa, Roger Yu',
+    license='MIT',
+)
