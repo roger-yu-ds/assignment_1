c72e6f171261426dc4e4232a9c5001235f475cbc
Mark Brackenrig <mark@brackenrig.net>
2021-02-02 19:57:59 +1100

    Template Notebook and helper functions
    * Added a template notebook for modelling
    * Added helper functions to visualise and submit predictions
    * reorganised notebooks folder.

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,29 +1,29 @@
 import sklearn.metrics as metr
-
-def classification_reports(classifier,X,y):
+import matplotlib.pyplot as plt
+def classification_reports(classifier,X,y, verbose = False):
     """
     Retrieved from: https://www.kaggle.com/asiyehbahaloo/asiyeh-bahaloo
     
-    Provides Confusion matrix, accuracy, AUC and standard classification report
+    Provides Confusion matrix, accuracy, AUC and standard classification report. Note Verbose returns ROC curve values and accuracy
     
     """
-    
-    size_data = len(labels)
-    count_class_1 = sum(labels)
+    y_pred = classifier.predict_proba(X)[:,1]
+    y_pred_lab = classifier.predict(X)
+    size_data = len(y)
+    count_class_1 = sum(y)
     count_class_0 = size_data - count_class_1
     print(' class 1 : ', count_class_1)
     print(' class 0 : ', count_class_0)
-    y = classifier.predict(features)
-    fpr, tpr, thresholds = metr.roc_curve(labels, y)
-    print("Confusion Matrix: \n",metr.confusion_matrix(labels,y))
-    score=metr.accuracy_score(labels,y)
+    fpr, tpr, thresholds = metr.roc_curve(y, y_pred)
+    print("Confusion Matrix: \n",metr.confusion_matrix(y,y_pred_lab))
+    score=metr.accuracy_score(y,y_pred_lab)
     print("Accuracy: ",score)
-    auc=metr.roc_auc_score(labels,y)
+    auc=metr.roc_auc_score(y,y_pred)
     print("AUC: ",auc)
-    print(metr.classification_report(labels,y))
+    print(metr.classification_report(y,y_pred_lab))
     plt.figure()
     plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     plt.plot( fpr, tpr,color='darkorange')
     plt.show()
-
-    return fpr,tpr,score
+    if verbose:
+        return fpr,tpr,score
